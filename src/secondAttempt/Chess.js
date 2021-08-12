64cd124dd39bfa0078a1d8e2c4d84c3f613c9abf
Svetlin <svetlimladenov@gmail.com>
2021-08-12 14:18:17 +0300

    Make the board state immutable and enable pawn plays

--- src/secondAttempt/Chess.js
+++ src/secondAttempt/Chess.js
@@ -1,3 +1,4 @@
+import { Map, List } from "../../node_modules/immutable/dist/immutable.es.js";
 import Event from "./Event.js";
 import Pawn from "./Pawn.js";
 
@@ -5,9 +6,10 @@ class Chess {
     constructor() {
         this.showPossibleMovesEvent = new Event();
         this.figuresCreatedEvent = new Event();
+        this.moveFigureEvent = new Event();
 
-        this._boards = [Chess.makeEmptyBoard()];
-        this.availableMoves = [];
+        this.immutableBoard = Chess.makeImmutableBoard();
+        this.availableMoves = { moves: [], figure: null };
         this.isStarted = false;
     }
 
@@ -17,39 +19,85 @@ class Chess {
             return;
         }
 
-        this._boards.push(this._addFigures());
+        this.immutableBoard = this._addFigures();
         this.isStarted = true;
     }
 
     restart() {
         this.isStarted = false;
-        this._updateBoard(Chess.makeEmptyBoard());
+        this.immutableBoard = Chess.makeImmutableBoard();
     }
 
     cellClicked(position) {
         const cell = this._getCellItem(position);
+        const playPositions = this._getPlay(position);
+
+        if (playPositions) {
+            const playedFigure = this.availableMoves.figure;
+
+            const updatedBoard = this.immutableBoard.map((row, rowIdx) => {
+                return row.map((col, colIdx) => {
+                    if (
+                        rowIdx === playPositions.y &&
+                        colIdx === playPositions.x
+                    ) {
+                        return playedFigure;
+                    }
+
+                    if (
+                        rowIdx === playedFigure.y &&
+                        colIdx === playPositions.x
+                    ) {
+                        return null;
+                    }
+
+                    return col;
+                });
+            });
+
+            this._updateBoard(updatedBoard);
+
+            const oldPosition = {
+                x: playedFigure.x,
+                y: playedFigure.y
+            };
+
+            this.availableMoves.figure.x = playPositions.x;
+            this.availableMoves.figure.y = playPositions.y;
+
+            this.moveFigureEvent.trigger(
+                this.availableMoves.figure,
+                this.availableMoves.moves.concat(oldPosition)
+            );
+
+            this.availableMoves = { moves: [], figure: null };
+        }
+
         if (cell) {
-            const newMoves = cell.getMoves(this._getLatestBoard());
-            const oldMoves = this.availableMoves;
+            const newMoves = cell.getMoves(this.immutableBoard).slice(); // make a copy so we dont pass the real reference
+            const oldMoves = this.availableMoves.moves.slice(); // make a copy so we dont pass the real reference
             this.showPossibleMovesEvent.trigger(oldMoves, newMoves);
-            this.availableMoves = newMoves;
+            this.availableMoves.moves = newMoves;
+            this.availableMoves.figure = cell;
         }
     }
 
-    _getLatestBoard() {
-        return this._boards[this._boards.length - 1];
+    _getCellItem(position) {
+        return this.immutableBoard.get(position.y).get(position.x);
     }
 
-    _getCellItem(position) {
-        return this._getLatestBoard()[position.y][position.x];
+    _getPlay(position) {
+        return this.availableMoves.moves.find(
+            (play) => play.x === position.x && play.y === position.y
+        );
     }
 
     _updateBoard(newBoard) {
-        this._boards.push(newBoard);
+        this.immutableBoard = newBoard;
     }
 
     _addFigures() {
-        const boardWithFigures = this._getLatestBoard().map((row, rowIdx) => {
+        const boardWithFigures = this.immutableBoard.map((row, rowIdx) => {
             let newRow = row;
             if (rowIdx === 1) {
                 newRow = row.map((_, idx) => {
@@ -64,9 +112,8 @@ class Chess {
         return boardWithFigures;
     }
 
-    static makeEmptyBoard() {
-        const board = Array(8).fill(Array(8).fill());
-        return board;
+    static makeImmutableBoard() {
+        return List(Array(8).fill(List(Array(8).fill(null))));
     }
 }
 
